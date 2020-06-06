3504fb91477b2c201022ae19cfe5a0ae766e7a95
beatricetierra <beatricetierra@gmail.com>
2020-06-05 17:16:40 -0700

    add scrollbar

--- LogDiagnosticTool.py
+++ LogDiagnosticTool.py
@@ -6,6 +6,7 @@ Created on Fri May  8 10:43:42 2020
 """
 import tkinter as tk
 from tkinter import filedialog, ttk
+import os
 from pandastable import Table
 import GetEntries as get
 
@@ -17,26 +18,30 @@ class LogDiagnosticToolTempalte():
         
         # ***Top Frame***
         self.topFrame = tk.Frame(master)
-        self.topFrame.place(relx=0.5, rely=0.02, relwidth=0.95, relheight=0.12, anchor='n')
+        self.topFrame.place(relx=0.5, rely=0.02, relwidth=0.98, relheight=0.12, anchor='n')
         
-        self.button1 = tk.Button(self.topFrame, text='Choose Files', font=30, bg='#D3D3D3', command=addFiles)
-        self.button1.place(relx=0.01, rely=0.03, relwidth=0.15, relheight=0.2)
+        self.button1 = tk.Button(self.topFrame, text='Choose Files', font=25, bg='#D3D3D3', command=addFiles)
+        self.button1.place(relx=0.01, rely=0.03, relwidth=0.1, relheight=0.2)
         
-        self.button2 = tk.Button(self.topFrame, text='Get Entries', font=30, bg='#D3D3D3', command=findEntries)
-        self.button2.place(relx=0.01, rely=0.25, relwidth=0.15, relheight=0.2)
+        self.button2 = tk.Button(self.topFrame, text='Get Entries', font=25, bg='#D3D3D3', command=findEntries)
+        self.button2.place(relx=0.01, rely=0.25, relwidth=0.1, relheight=0.2)
         
         self.labelFiles = tk.Label(self.topFrame, text='Files:', font=12, anchor='w')
-        self.labelFiles.place(relx=0.18, rely=0.01, relwidth=0.05, relheight=0.25)
+        self.labelFiles.place(relx=0.12, rely=0.01, relwidth=0.05, relheight=0.25)
+
+        # File List
+        self.scrollFrame = tk.Frame(self.topFrame, bd=1, relief='solid')
+        self.scrollFrame.place(relx=0.17, rely=0.01, relwidth=0.6, relheight=0.82)
         
         # Info Dashboard
-        self.infoFrame = tk.Frame(self.topFrame, bd=1, relief='solid')
-        self.infoFrame.place(relx=0.18, rely=0.3, relwidth=0.82, relheight=0.62)
+        # self.infoFrame = tk.Frame(self.topFrame, bd=1, relief='solid')
+        # self.infoFrame.place(relx=0.18, rely=0.3, relwidth=0.82, relheight=0.62)
         
-        self.labelInfo1 = tk.Label(self.infoFrame, text='Number of Total Interlocks:', font=12, anchor='w')
-        self.labelInfo1.place(relwidth=0.3, relheight=0.25)
+        # self.labelInfo1 = tk.Label(self.infoFrame, text='Number of Total Interlocks:', font=12, anchor='w')
+        # self.labelInfo1.place(relwidth=0.3, relheight=0.25)
         
-        self.labelInfo2 = tk.Label(self.infoFrame, text='Number of Unexpected Interlocks:', font=12, anchor='w')
-        self.labelInfo2.place(rely=0.4, relwidth=0.3, relheight=0.25)
+        # self.labelInfo2 = tk.Label(self.infoFrame, text='Number of Unexpected Interlocks:', font=12, anchor='w')
+        # self.labelInfo2.place(rely=0.4, relwidth=0.3, relheight=0.25)
         
         # ***Bottom Frame***
         self.bottomFrame = tk.Frame(master)
@@ -60,7 +65,8 @@ def addFiles():
     
     root.update()
     files = filedialog.askopenfilenames(parent=app.topFrame,title='Choose files', defaultextension='.log')
-    labelLog.config(text=files)
+    file_list = "\n".join(map(str,files))
+    textbox.insert(tk.END, file_list)
     
 def findEntries():
     statusbar.config(text='Loading...')
@@ -85,23 +91,22 @@ root = tk.Tk()
 app = LogDiagnosticToolTempalte(root)
 
 # Top Frame
-labelLog = tk.Label(app.topFrame, anchor='w', bg='#D0D3D4', relief='sunken')
-labelLog.place(relx=0.22, rely=0.03, relwidth=0.8, relheight=0.2)
+scrollbar = tk.Scrollbar(app.scrollFrame)
+scrollbar.pack(side='right', fill='y')
 
-labelInfo3 = tk.Label(app.infoFrame, anchor='w')
-labelInfo3.place(relx=0.18, relwidth=0.1, relheight=0.25)
+textbox = tk.Text(app.scrollFrame, height = 500, width = 350, yscrollcommand=scrollbar.set)
+textbox.pack(expand=0, fill='both')
+scrollbar.config(command=textbox.yview)
 
-labelInfo4 = tk.Label(app.infoFrame, anchor='w')
-labelInfo4.place(relx=0.22, rely=0.4, relwidth=0.1, relheight=0.25)
+
+# labelInfo3 = tk.Label(app.infoFrame, anchor='w')
+# labelInfo3.place(relx=0.18, relwidth=0.1, relheight=0.25)
+
+# labelInfo4 = tk.Label(app.infoFrame, anchor='w')
+# labelInfo4.place(relx=0.22, rely=0.4, relwidth=0.1, relheight=0.25)
 
 # Bottom Frame
 statusbar = tk.Label(app.bottomFrame, bd=1, relief='sunken', anchor='w')
 statusbar.pack(side='bottom', fill='x')
 
 root.mainloop()
-
-
-
-
-
-
