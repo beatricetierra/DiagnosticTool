605b80041555576ab6c46f9d8bc085aed64712ba
beatricetierra <beatricetierra@gmail.com>
2020-06-22 18:09:49 -0700

    update graph displays

--- DiagnosticTool_Graphs.py
+++ DiagnosticTool_Graphs.py
@@ -4,6 +4,7 @@ Created on Mon Jun 22 13:42:58 2020
 
 @author: btierra
 """
+import matplotlib.pyplot as plt
 
 def filtered_graphing(filtered_analysis):
      # Shorten Interlock Number Description for graphing
@@ -12,14 +13,15 @@ def filtered_graphing(filtered_analysis):
     except:
         pass
     
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15,5))
     # Total count vs interlocks
     count = filtered_analysis.groupby('Interlock Number').sum()['Count']
-    count.plot(kind='bar', title='Total Count per Interlock')
+    count.plot(kind='bar', title='Total Count per Interlock', ax=ax1)
     
     # Start of Node and Interlock Duration Times
     average = filtered_analysis.groupby('Interlock Number').sum()['Avg Duration(sec)']
     std = filtered_analysis.groupby('Interlock Number').sum()['Std Duration(sec)']
-    average.plot(kind='bar', yerr=std)
+    average.plot(kind='bar', yerr=[std], title='AVG Interlock Duration', ax=ax2)
     
 def unfilt_graphing(unfilt_analysis):
     # Shorten Interlock Number Description for graphing
@@ -28,19 +30,20 @@ def unfilt_graphing(unfilt_analysis):
     except:
         pass
     
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15,5))
     # Total count vs interlocks
     count = unfilt_analysis.groupby('Interlock Number').sum().iloc[:,:3]
-    count.plot(kind='bar', title='Total Count per Interlock')
+    count.plot(kind='bar', title='Total Count per Interlock', ax=ax1)
     
     # total count vs session
     session = unfilt_analysis.groupby('Session').sum().iloc[:,:3]
-    session.plot(kind='bar', title='Total Interlocks per Session')
+    session.plot(kind='bar', title='Total Interlocks per Session', ax=ax2)
     
     # total count vs interlocks per session
     sessions = list(set(unfilt_analysis['Session']))
+    
     for session in sessions:
         df = unfilt_analysis.loc[unfilt_analysis['Session'] == session].iloc[:,:5]
         df = df.groupby('Interlock Number').sum()
         df.plot(kind='bar', title=str(session) + ' Total Interlocks')
         
-
