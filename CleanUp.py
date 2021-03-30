0a11faeb455a8f58afe146283764c12e8ee1aace
beatricetierra <beatricetierra@gmail.com>
2021-03-29 23:17:11 -0700

    allow to pass through CleanUp functions if not included in json setup file

--- CleanUp.py
+++ CleanUp.py
@@ -15,18 +15,21 @@ def CleanDataframes(interlocks_per_node_event_details):
     dfs = RenameColumns()
     dfs = FormatDatetimeColumns()
     dfs = ReorderColumns()
-    dfs = ParseColumns()
+    ParseColumns()
     dfs = ReadAsJSON()
     return dfs
 
 def RenameColumns():
-    renamed_dfs = {}
-    for node, df in dfs.items():
-        column_names = var.column_names[node]
-        for col_original, col_new in zip(df.columns[7:], column_names):
-            df = df.rename(columns={col_original:col_new})
-        renamed_dfs[node] = df
-    return renamed_dfs
+    try:
+        renamed_dfs = {}
+        for node, df in dfs.items():
+            column_names = var.column_names[node]
+            for col_original, col_new in zip(df.columns[7:], column_names):
+                df = df.rename(columns={col_original:col_new})
+            renamed_dfs[node] = df
+        return renamed_dfs
+    except:
+        return dfs
 
 def FormatDatetimeColumns():
     for node, df in dfs.items():
@@ -43,27 +46,33 @@ def FormatDatetimeColumns():
 
 def ReorderColumns():
     # Move fixed columns in the front
-    reorderd_dfs = {}
-    for node, df in dfs.items():
-        fixed_columns = var.fixed_columns
-        add_columns = var.column_names[node]
-        df = df[fixed_columns + add_columns]
-        reorderd_dfs[node] = df
-    return reorderd_dfs
+    try:
+        reorderd_dfs = {}
+        for node, df in dfs.items():
+            fixed_columns = var.fixed_columns
+            add_columns = var.column_names[node]
+            df = df[fixed_columns + add_columns]
+            reorderd_dfs[node] = df
+        return reorderd_dfs
+    except:
+        return dfs 
 
 def ParseColumns():
     for node, df in dfs.items():
-        columns_to_parse = var.parse_column_entries[node]
-        for col, entry_parse_struct in columns_to_parse.items():
-            if col in df.columns:
-                for idx, entry in enumerate(df[col]):
-                    match = re.search(entry_parse_struct, entry)
-                    if type(match) != type(None):
-                        wildcard = match.group(1)
-                        df[col].iloc[idx] = wildcard
-                    else:  # no matching objects found
-                        df[col].iloc[idx] = entry
-    return dfs
+        try:
+            columns_to_parse = var.parse_column_entries[node]   #within try in case no parse formatting given in json file
+            for col, entry_parse_struct in columns_to_parse.items():
+                if col in df.columns:
+                    for idx, entry in enumerate(df[col]):
+                        match = re.search(entry_parse_struct, entry)
+                        if type(match) != type(None):
+                            wildcard = match.group(1)
+                            df[col].iloc[idx] = wildcard
+                        else:  # no matching objects found
+                            df[col].iloc[idx] = entry
+        except:
+            pass
+    
 
 def ReadAsJSON():
     for node, df in dfs.items():
